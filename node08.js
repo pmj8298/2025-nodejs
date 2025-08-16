1065efcb1087973fb4ba980ac58a6f3ac7797238
pmj8298 <164135340+pmj8298@users.noreply.github.com>
2025-08-16 16:33:32 +0900

    폴더와 파일이름 구분한 리스트 띄우기

--- node08.js
+++ node08.js
@@ -4,15 +4,21 @@ const fs = require('fs')
 const app = express()
 const port = 3000
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname)) // __dirname : 현재 실행 중인 JS 파일이 위치한 디렉토리 경로
 
 app.get('/',(req,res)=>{
-    fs.readdir(__dirname, 'utf-8', (err,data)=>{
+    fs.readdir(__dirname, {withFileTypes:true, encoding:'utf-8'}, (err,data)=>{
         let list = `<h1>링크를 선택하세요</h1><ul>`
 
+        // 위에 {withFileTypes:true} 넣으면 fs.stat ... 생략가능
         data.forEach(v=>{
 
-            list += `<li><a href="${v}">${v}</a></li>`
+            // list += `<li><a href="${v}" download>${v}[download]</a></li>` // html은 뒤에 download를 적어주면 다운 가능함
+            if(v.isFile()){
+                list += `<li><a href="${v.name}" >${v.name}</a></li>` // 클릭하면 파일 안의 내용을 바로 볼 수 있음
+            }else{
+                list += `<li>[폴더]${v.name}</li>`
+            }
         })
         
         list += '</ul>'
